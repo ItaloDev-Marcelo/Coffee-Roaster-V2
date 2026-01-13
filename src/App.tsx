d27877d28ce9e2a3180773af8f143df98da12249
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-01-13 18:10:32 -0300

    feat: add componets, NavBar RouteApp to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react"
+import NavBar from "./components/Nav/NavBar"
+import RouteApp from "./routes/RouteApp"
+import Logo from './assets/images/shared/desktop/logo.svg';
+import humburger from './assets/images/shared/mobile/icon-hamburger.svg';
 
 function App() {
+
+  const [navActive, setNavActive] = useState(true)
+  const handleNav = () => setNavActive(!navActive)
+
   return (
     <>
-      <p className="read-the-docs bg-red-400 text-white">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav className='flex flex-row lg:hidden h-20 justify-between items-center p-5'>
+                   <a href='#'><img src={Logo} alt='logo' className='w-37.5'/> </a> 
+                   <button onClick={handleNav}><img src={humburger} alt='humburger' /></button> 
+      </nav>
+
+      {!navActive ? <div className='lg:hidden'><NavBar handleNav={handleNav} type={true} /></div> : null}
+      <div className='hidden lg:flex'><NavBar handleNav={handleNav} type={false} /></div>
+      {navActive ? <RouteApp/> : null}
     </>
   )
 }
