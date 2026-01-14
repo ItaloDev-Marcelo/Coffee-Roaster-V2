c13e6d4ae6243810f587f64234b679dbb33120ab
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-01-14 13:58:38 -0300

    style: add padding-left to NavLink.tsx

--- src/components/Nav/Nav-link/NavLink.tsx
+++ src/components/Nav/Nav-link/NavLink.tsx
@@ -4,7 +4,9 @@ import type { NavlinkProps } from '../../../types/Nav.type'
 const NavLink = ({path, name, handleNav, type}: NavlinkProps) => {
   return (
     <>
-      {type ? <li onClick={handleNav} className='my-3 lg:my-0 lg:mx-2 font-semibold text-gray-400 text-lg cursor-pointer hover:text-gray-600 '><Link to={path}>{name}</Link></li> :
+      {type ? 
+      <li onClick={handleNav} className='my-3 pr-14 lg:pr-0 lg:my-0 lg:mx-2 font-semibold text-gray-400
+       text-lg cursor-pointer hover:text-gray-600 '><Link to={path}>{name}</Link></li> :
      <li  className='my-2 lg:my-0 lg:mx-4 font-semibold text-gray-400 cursor-pointer hover:text-gray-600'><Link to={path}>{name}</Link></li>}
     </>
   )
