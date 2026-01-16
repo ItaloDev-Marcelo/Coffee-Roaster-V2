923ac75ad44cc3c9cf2152542db6c7075cade2e4
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-01-16 15:08:20 -0300

    feat: create Hero to render Banner img

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,35 @@
+import type { HeroProps } from "../../types/hero.type";
+
+const Hero = ({mobile, tablet, desk, type}:HeroProps) => {
+  switch (type) {
+     case 'Home':
+        return (
+            <>
+               <img src={mobile} alt="mobile-hero" className='md:hidden rounded-xl' />
+               <img src={tablet} alt="tablet-hero" className='hidden md:flex lg:hidden rounded-xl' />
+               <img src={desk} alt="desktop-hero" className='hidden lg:flex rounded-xl w-[98%]' />
+            </>
+        )
+     break; 
+       case 'About':
+        return (
+            <>
+               <img src={mobile} alt="mobile-hero" className='md:hidden rounded-xl' />
+               <img src={tablet} alt="tablet-hero" className='hidden md:flex lg:hidden rounded-xl' />
+               <img src={desk} alt="desktop-hero" className='hidden lg:flex rounded-xl  w-[98%]' />
+            </>
+        )
+     break; 
+       case 'Plan':
+        return (
+            <>
+              <img src={mobile} alt="mobile-hero" className='md:hidden rounded-xl' />
+               <img src={tablet} alt="tablet-hero" className='hidden md:flex lg:hidden rounded-xl' />
+               <img src={desk} alt="desktop-hero" className='hidden lg:flex rounded-xl  w-[98%]' />
+            </>
+        )
+     break; 
+  }
+}
+
+export default Hero
