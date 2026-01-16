21e584c9593e3717bc7550f91b073a9d830e4fd0
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-01-16 15:12:42 -0300

    feat: add Hero.type to all hero components

---
+++ src/types/hero.type.ts
@@ -0,0 +1,26 @@
+
+export type HeroImages = {
+    mobile: string; 
+    tablet: string,
+    desk: string, 
+}
+
+export type HeroType = {
+    type: string 
+}
+
+export type HeroProps = HeroImages & HeroType
+
+export type HeroContainerProps = {
+    photoType: HeroImages,
+    title: string,
+    subtitle: string,
+    btn: string,
+    btnActive: boolean
+} & HeroType
+
+export type HeroBankType = {
+    Home: HeroImages,
+    About: HeroImages,
+    Plan: HeroImages
+}
