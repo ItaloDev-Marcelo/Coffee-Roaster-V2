77ff9f2e39fdd1e3da9c7f539a236f9d2cddbcab
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-01-21 14:37:26 -0300

    feat: create NumData

---
+++ src/components/Data/NumData.ts
@@ -0,0 +1,49 @@
+
+
+const t1 = 'Pick your coffee';
+const t2 = 'Choose the frequency'
+const t3 = 'Receive and enjoy!'
+
+const d1 = 'Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.';
+const d2 = 'Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.';
+const d3 = 'We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.';
+
+export const WhiteData = [
+    {
+        num: '01',
+        title: t1,
+        description: d1,
+        hasAButton: false
+    },
+     {
+        num: '02',
+        title: t2,
+        description: d2,
+        hasAButton: false
+    },
+    {
+        num: '03',
+        title: t3,
+        description: d3,
+        hasAButton: true
+    }
+]
+
+export const BlackData = [
+    {
+        num: '01',
+        title: t1,
+        description: d1
+    },
+     {
+        num: '02',
+        title: t2,
+        description: d2
+    },
+    {
+        num: '03',
+        title: t3,
+        description: d3,
+        btn: false
+    }
+]
