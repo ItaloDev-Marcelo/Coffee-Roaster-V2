3a25fa662b3648c08c74f6016a78bc78b203833c
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-01-21 14:48:25 -0300

    feat: create WhyChooseData

---
+++ src/components/Data/Whychoose.ts
@@ -0,0 +1,30 @@
+import CoffeeBean from '../../assets/images/home/desktop/icon-coffee-bean.svg';
+import GifBox from '../../assets/images/home/desktop/icon-gift.svg';
+import Truck from '../../assets/images/home/desktop/icon-truck.svg';
+
+export const WhyChoose = [
+     {
+        style: 'bg-blue-600 w-[340px] lg:w-[300px] h-[420px] my-5 lg:mx-5 lg:my-0 p-7 rounded-lg flex flex-col justify-center items-center text-white' ,
+        space: false,
+        imgSpace: 'mt-[-3rem] pb-10',
+        icon: CoffeeBean,
+        title:'Best quality',
+        description: 'Discover an endless variety of the world’s best artisan coffee from each of our roasters.'
+     },
+      {
+      style: 'bg-blue-600 w-[340px] lg:w-[300px] h-[420px] my-5 lg:mx-5 lg:my-0 p-7 rounded-lg flex flex-col justify-center items-center text-white' ,
+        space: false,
+        imgSpace: 'mt-[-3rem] pb-10',  
+        icon: GifBox,
+        title:'Exclusive benefits',
+        description: 'Special offers and swag when you subscribe, including 30% off your first shipment.'
+     },
+      {
+        style: 'bg-blue-600 w-[340px] lg:w-[300px] h-[420px] my-5 lg:mx-5 lg:my-0 p-7 rounded-lg flex flex-col justify-center items-center text-white' ,
+        space: false,
+        imgSpace: 'mt-[-3rem] pb-10',   
+        icon: Truck,
+        title:'Free shipping',
+        description: 'We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.'
+     }
+]
