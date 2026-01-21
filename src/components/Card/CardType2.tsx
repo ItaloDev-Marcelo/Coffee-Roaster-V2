69a36fa3b0163d79a8c464db9ac6786087ee95d9
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-01-21 14:45:00 -0300

    style: add style to CardType2

---
+++ src/components/Card/CardType2.tsx
@@ -0,0 +1,22 @@
+import Button from "../Plan-Button/Button"
+
+
+export type CardType2Props = {
+    num: string | number;
+    title: string;
+    description: string;
+    hasAButton: boolean;
+}
+
+const CardType2 = ({num, title, description, hasAButton}:CardType2Props) => {
+  return (
+    <article className='w-[330px] lg:w-[300px]  my-4 lg:my-0 lg:mx-0'>
+         <h3 className='font-bold text-[7rem] lg:text-[6.5rem]'>{num}</h3>
+         <h4 className="text-2xl font-semibold w-[150px] lg:w-[300px]">{title}</h4>
+         <p className="my-5">{description}</p>
+         {hasAButton && <Button/>}
+    </article>
+  )
+}
+
+export default CardType2
