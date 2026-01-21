30cbfa763b8477e6cd5b5541ca8557a1a8b81e6c
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-01-21 14:38:26 -0300

    feat: create OurCollectionData

---
+++ src/components/Data/OurCollectionData.ts
@@ -0,0 +1,42 @@
+import GranEspresso from '../../assets/images/home/desktop/image-gran-espresso.png';
+import Planalto from '../../assets/images/home/desktop/image-planalto.png';
+import Piccollo from '../../assets/images/home/desktop/image-piccollo.png';
+import Danche from '../../assets/images/home/desktop/image-danche.png';
+
+// style, space, imgSpace,icon, title,description
+
+
+export const OurCollectionData = [
+    {
+        style: 'w-[200px] lg:w-[250px] text-center flex flex-col items-center justify-center my-7 lg:my-5 xl:my-0 z-1000' ,
+        space: false,
+        imgSpace: 'w-[150px] lg:w-[200px] my-2 lg:pb-20 xl:my-5',
+        icon:GranEspresso,
+        title:'Gran Espress',
+        description: 'Light and flavorful blend with cocoa and black pepper for an intense experience'
+    },
+    {
+         style: 'w-[200px] lg:w-[250px] text-center flex flex-col items-center justify-center my-7 lg:my-5 xl:my-0 z-1000 ',
+        space: false,
+        imgSpace: 'w-[150px] lg:w-[200px] my-2 lg:pb-20 xl:my-5',
+        icon:Planalto,
+        title:'Planalto',
+        description: 'Brazilian dark roast with rich and velvety body, and hints of fruits and nuts'
+    },
+    {
+         style: 'w-[200px] lg:w-[250px] text-center flex flex-col items-center justify-center my-7 lg:my-5 xl:my-0 z-1000 ',
+        space: false,
+        imgSpace: 'w-[150px] lg:w-[200px] my-2 lg:pb-20 xl:my-5',
+        icon:Piccollo,
+        title:'Piccollo',
+        description: 'Mild and smooth blend featuring notes of toasted almond and dried cherry'
+    },
+    {
+         style: 'w-[200px] lg:w-[250px] text-center flex flex-col items-center justify-center my-7 lg:my-5 xl:my-0 z-1000 ',
+        space: true,
+        imgSpace: 'w-[150px] lg:w-[200px] my-2 lg:pb-20 xl:my-5',
+        icon:Danche,
+        title:'Danche',
+        description: 'Ethiopian hand-harvested blend densely packed with vibrant fruit notes'
+    }
+]
