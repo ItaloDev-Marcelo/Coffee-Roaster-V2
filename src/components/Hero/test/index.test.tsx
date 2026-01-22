ce105907e3266714adc5d2111e4dea7b849cbb19
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-01-22 14:27:32 -0300

    refactore: add MemoryRouter to test

--- src/components/Hero/test/index.test.tsx
+++ src/components/Hero/test/index.test.tsx
@@ -2,11 +2,18 @@ import {HeroBank} from '../utils/HeroBank';
 import Hero from '../Hero'
 import HeroSection from '../HeroSection';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'
+
+export const renderWithRouter = (ui: React.ReactElement) => {
+        return render(<MemoryRouter>{ui}</MemoryRouter>);
+}
 
 describe('testing if the images files will start or not: ', () => {
    it('should render Hero component with Home images', () => {
     const {mobile, tablet, desk} = HeroBank.Home;
-    const {getByRole} = render(<Hero mobile={mobile} tablet={tablet} desk={desk} type='Home' />);
+    const {getByRole} = renderWithRouter(<Hero mobile={mobile} tablet={tablet} desk={desk} type='Home' />);
+    
+
     getByRole('img', { name: /mobile-hero/i });
     getByRole('img', { name: /tablet-hero/i });
     getByRole('img', { name: /desktop-hero/i });
@@ -15,11 +22,12 @@ describe('testing if the images files will start or not: ', () => {
 
 describe('render HeroSection', () => {
    it('should render HeroSection with a type:' , () => {
-     render(<HeroSection photoType={HeroBank.Home} type='Home' title='Welcome' 
-      subtitle='This is the hero section' btn='Get Started' btnActive={true} />);
+      renderWithRouter(<HeroSection photoType={HeroBank.Home} type='Home' title='Welcome' 
+      subtitle='This is the hero section'  btnActive={true} />);
 
       screen.getByText('Welcome');
       screen.getByText('This is the hero section');
-      screen.getByRole('button', {name: /Get Started/i});
+
+      screen.getByText('Get Started');
    })
 })
