17add872f21acfdb0e8970a0aac3a2317e5ea09d
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-01-27 13:48:34 -0300

    feat: create ImageBank to aboutpage

---
+++ src/pages/about/utils/ImageBank.ts
@@ -0,0 +1,21 @@
+import CommitmentDesk from '../../../assets/images/about/desktop/image-commitment.jpg';
+import CommitmentMobile from '../../../assets/images/about/mobile/image-commitment.jpg';
+import CommitmentTablet from '../../../assets/images/about/tablet/image-commitment.jpg';
+
+import qualityDesk from '../../../assets/images/about/desktop/image-quality.jpg';
+import QualityMobile from '../../../assets/images/about/mobile/image-quality.jpg';
+import QualityTablet from '../../../assets/images/about/tablet/image-quality.jpg';
+
+
+export const ImageBank = {
+    Commitment: {
+        mobile: CommitmentMobile,
+        tablet: CommitmentTablet,
+        desk: CommitmentDesk
+    },
+    Quality: {
+        mobile: QualityMobile,
+        tablet: QualityTablet,
+        desk: qualityDesk
+    }
+}
