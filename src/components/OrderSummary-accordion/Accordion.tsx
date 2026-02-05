88e6b8c761ae9b4c792f7b7ad39d682b1ac23910
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-05 20:00:19 -0300

    refactor: remove hanldeAccirdion function and AsideMenu component

--- src/components/OrderSummary-accordion/Accordion.tsx
+++ src/components/OrderSummary-accordion/Accordion.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import AccordionItem from './AccordionItem'
 import AccordionOption from './AccordionOption'
 import { accordionData0 } from './Data/AccordionData'
-import AsideMenu from './AsideMenu/AsideMenu'
 import type { SubInfoType } from '../../types/Accordion.types'
 
-const Accordion = () => {
+type AccType = {
+  handleAccordion: (campo: number | null) => void;
+  accordionOpen: number | null
+}
 
-
-  const [accordionOpen, setAccordionOpen] = useState<number|null>(null)
+const Accordion = ({handleAccordion,accordionOpen}:AccType) => {
 
   const [subInfo, setSubInfo] = useState<SubInfoType>({
     item1: '',
@@ -18,9 +19,6 @@ const Accordion = () => {
     item5: ''
   })
 
-
-  const handleAccordion = (campo: number | null) =>
-  accordionOpen === campo ? setAccordionOpen(null) : setAccordionOpen(campo)
  
   const handleInfo = (name: string, title: string) =>  setSubInfo(prev => ({
     ...prev,
@@ -32,7 +30,7 @@ const Accordion = () => {
 
     <div>    
       <div>
-        <AsideMenu handleAccordion={handleAccordion} />
+       
       </div>
          <AccordionItem handleAccordion={handleAccordion} data={accordionData0[0]} index={0} accordionOpen={accordionOpen}> 
            {
@@ -41,6 +39,7 @@ const Accordion = () => {
              ))
            }
          </AccordionItem>
+         <hr className='opacity-25'/>
          <AccordionItem handleAccordion={handleAccordion} data={accordionData0[1]} index={1} accordionOpen={accordionOpen}> 
            {
              accordionData0[1].accordionItems.map(item => (
@@ -48,6 +47,7 @@ const Accordion = () => {
              ))
            }
          </AccordionItem>
+           <hr className='opacity-25'/>
          <AccordionItem handleAccordion={handleAccordion} data={accordionData0[2]} index={2} accordionOpen={accordionOpen}> 
            {
              accordionData0[2].accordionItems.map(item => (
@@ -55,6 +55,7 @@ const Accordion = () => {
              ))
            }
          </AccordionItem>
+           <hr className='opacity-25'/>
          <AccordionItem handleAccordion={handleAccordion} data={accordionData0[3]} index={3} accordionOpen={accordionOpen}> 
            {
              accordionData0[3].accordionItems.map(item => (
@@ -62,6 +63,7 @@ const Accordion = () => {
              ))
            }
          </AccordionItem>
+           <hr className='opacity-25'/>
          <AccordionItem handleAccordion={handleAccordion} data={accordionData0[4]} index={4} accordionOpen={accordionOpen}> 
            {
              accordionData0[4].accordionItems.map(item => (
