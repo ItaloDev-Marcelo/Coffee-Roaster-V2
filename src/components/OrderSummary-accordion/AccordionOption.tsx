3fbef6707919f0c0eaaef3303e51a03aa8a5a84b
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-05 20:01:10 -0300

    style: add style to AccordionOption

--- src/components/OrderSummary-accordion/AccordionOption.tsx
+++ src/components/OrderSummary-accordion/AccordionOption.tsx
@@ -3,8 +3,10 @@ import type { AccordionOptionProps } from "../../types/Accordion.types"
 const AccordionOption = ({handleInfo, data, subInfo}:AccordionOptionProps) => {
   const {title,item,text} = data
   return (
-    <div onClick={() => handleInfo( item,title)} className={subInfo === title ? 'bg-red-400' : 'bg-pink-400'} >
-        <h2>{title}</h2>
+    <div onClick={() => handleInfo( item,title)} 
+    className={subInfo === title ? 'bg-teal-700 text-white p-4 rounded-md  cursor-pointer' : 
+    'hover:bg-orange-200 text-black cursor-pointer p-4 bg-white shadow-md rounded-md'} >
+        <h2 className="text-2xl font-bold mb-2">{title}</h2>
         <p>{text}</p>
     </div>
   )
