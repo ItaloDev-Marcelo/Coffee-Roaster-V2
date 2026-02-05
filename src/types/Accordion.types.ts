4ac2a5a3f62d20468ceba621a2f4c368d14e7a05
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-05 10:41:39 -0300

    feat: Accordion.type.ts to holder all types to Accordion components

---
+++ src/types/Accordion.types.ts
@@ -0,0 +1,35 @@
+
+import type React from 'react';
+export type SubInfoType = Record<string, string>
+
+export type subProps = {
+  title: string,
+  text: string,
+  item: string
+}
+
+export type AccProps = {
+  accordionTitle: string,
+  accordionItems: subProps[]
+}
+
+
+export type AccSubProps = {
+  title: string,
+  text: string
+  item: string, 
+}
+
+export type AccordionItemProps = {
+    handleAccordion: (number: number) => void,
+    index: number,
+    data: AccProps
+    accordionOpen: number | null,
+    children: React.ReactNode
+}
+
+export type AccordionOptionProps = {
+    handleInfo: (name: string, title: string) => void;
+    data: subProps,
+    subInfo: string
+}
