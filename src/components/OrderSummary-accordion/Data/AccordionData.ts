dbb261ac4e2ce6e0cca81d9bc4288c935cdf8e70
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-05 10:58:57 -0300

    feat: create AccordionData to hold all information to use on my Accordions components

---
+++ src/components/OrderSummary-accordion/Data/AccordionData.ts
@@ -0,0 +1,102 @@
+export const accordionData0 = [
+    {
+      accordionTitle: 'How do you drink you coffee?',
+      accordionItems: [
+        {
+          title: 'Capsule',
+          text: 'Compatible with Nespresso systems and similar brewers',
+          item: 'item1'
+        },
+        {
+          title: 'Filter',
+          text: 'For pour over or drip methods like Aeropress, Chemex, and V60',
+          item: 'item1'
+        },
+        {
+          title: 'Expresso',
+          text: 'Dense and finely ground benas for an intense, flavorful experience',
+          item: 'item1'
+        } 
+      ]
+    },
+      {
+      accordionTitle: 'What type of coffee?',
+      accordionItems: [
+        {
+          title: 'Single Origin',
+          text: 'Distinct, hight quality coffee from a specific family-owned farm',
+          item: 'item2'
+        },
+        {
+          title: 'Decaf',
+          text: 'Just like regular coffee, expect the caffeine has been removed',
+          item: 'item2'
+        },
+        {
+          title: 'Blended',
+          text: 'Combination oof two or three dark roasted beans of organic coffees',
+          item: 'item2'
+        } 
+      ]
+    },
+      {
+      accordionTitle: 'How much would you like ?',
+      accordionItems: [
+        {
+          title: '250g',
+          text: 'Perfect for the solo drinker. Yields about 12 delicius coups.',
+          item: 'item3'
+        },
+        {
+          title: '500g',
+          text: 'Perfect option for a couple. Yields about 40 delectable cups.',
+          item: 'item3'
+        },
+        {
+          title: '1000g',
+          text: 'Perfect for offices and events. Yields about 90 delightfull cups.',
+          item: 'item3'
+        } 
+      ]
+    },
+      {
+      accordionTitle: 'Want us to grind them ?',
+      accordionItems: [
+       {
+          title: 'Wholebean',
+          text: 'Best choice if you chersih the full sensory experience',
+          item: 'item4'
+       },
+        {
+          title: 'Filter',
+          text: 'For drip or pour-over coffee methods such as V60 or Aeropress',
+          item: 'item4'
+        },
+        {
+          title: 'Cafetiére',
+          text: 'Course ground beans specially suited for french press coffee',
+          item: 'item4'
+        } 
+      ]
+    },
+     {
+      accordionTitle: 'How often should we deliver',
+      accordionItems: [
+       {
+          title: 'Every week',
+          text: '$7.20 per shipment. Includes free First-class shipping',
+          item: 'item5'
+       },
+        {
+          title: 'Every 2 weeks',
+          text: '$9.60 per shipment. Includes free priority shipping',
+          item: 'item5'
+        },
+        {
+          title: 'Every Month',
+          text: '$12 per shipment. Includes free priority shipping',
+          item: 'item5'
+        } 
+      ]
+    }
+  ]
