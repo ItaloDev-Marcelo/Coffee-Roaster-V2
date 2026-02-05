1239a95663f31f6134b0c5478b6be51ac5092c9d
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-05 11:06:32 -0300

    feat: add more content to CreateYourPlan page

--- src/pages/plan/index.tsx
+++ src/pages/plan/index.tsx
@@ -1,6 +1,9 @@
 import HeroContainer from '../../components/Hero/HeroSection'
 import { HeroBank } from '../../components/Hero/utils/HeroBank'
-
+import Steps from '../../assets/images/plan/desktop/bg-steps.png'
+import { BlackData } from '../../components/Data/NumData'
+import CustomCard from '../../components/Card/CustomCard'
+import Accordion from '../../components/OrderSummary-accordion/Accordion'
 
 const CreateYourPlan = () => {
 
@@ -12,6 +15,19 @@ const CreateYourPlan = () => {
        <main className='flex flex-col justify-center items-center '>
                 <HeroContainer photoType={HeroBank.Plan} type='Plan'
                  title={title} subtitle={subtitle}  btnActive={false} />
+                 <section className='relative px-5 lg:p-14.5 lg:left-[-1em]  my-7 text-white'>
+                    <img src={Steps} className='w-screen  h-[240vh]  lg:h-[90vh] rounded-lg' />
+                     <div className=' flex flex-col lg:flex-row my-2 lg:my-0 gap-14 absolute top-25 left-25 lg:left-30 lg:top-50'>
+                                     {
+                                      BlackData.map((item,index) => (
+                                         <CustomCard key={index} customType={2} data2={item}  />
+                                      ))
+                                     }
+                                  </div>
+                 </section>
+                 <section className='bg-red-500'>
+                     <Accordion />
+                 </section>
         </main>
     </>
   )
