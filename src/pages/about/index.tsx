d2b9a47c928637ffe02d7f0bc9eef1d0ed095d08
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-08 16:04:45 -0300

    refactor: remove title,substitle,p1,p2 and import from AboutUs-text

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,18 +1,13 @@
 import CustomImage from '../../components/CustomImage/CustomImage'
 import HeroContainer from '../../components/Hero/HeroSection'
 import { HeroBank } from '../../components/Hero/utils/HeroBank'
+import { p1, p2, subtitle, title } from './utils/AboutUs-text'
 import { HeadquartersData } from './utils/headquartersData'
 import { ImageBank } from './utils/ImageBank'
 
-
 const AboutUs = () => {
 
-  const title = 'About Us'
-  const subtitle = 'Coffeeroasters began its journey of exotic discovery in 1999, highlighting stories of coffee from around the world. We have since been dedicated to bring the perfect cup - from bean to brew - in every shipment.'
 
-
-  const p1 = 'We’re built on a simple mission and a commitment to doing good along the way. We want to make it easy for you to discover and brew the world’s best coffee at home. It all starts at the source. To locate the specific lots we want to purchase, we travel nearly 60 days a year trying to understand the challenges and opportunities in each of these places. We collaborate with exceptional coffee growers and empower a global community of farmers through with well above fair-trade benchmarks. We also offer training, support farm community initiatives, and invest in coffee plant science. Curating only the finest blends, we roast each lot to highlight tasting profiles distinctive to their native growing region.'
-  const p2 = 'Although we work with growers who pay close attention to all stages of harvest and processing, we employ, on our end, a rigorous quality control program to avoid over-roasting or baking the coffee dry. Every bag of coffee is tagged with a roast date and batch number. Our goal is to roast consistent, user-friendly coffee, so that brew ing is easy and enjoyable.'
   return (
     <>
       <main className='flex flex-col justify-center items-center '>
