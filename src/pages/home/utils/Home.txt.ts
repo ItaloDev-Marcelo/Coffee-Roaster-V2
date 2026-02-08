926e3ed62d46b20ec7d93310eb9b2b8e2a2ffffd
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-08 16:08:47 -0300

    feat: create Home.txt component

---
+++ src/pages/home/utils/Home.txt.ts
@@ -0,0 +1,2 @@
+  export const HomeTitle = 'Great coffee made simple.'
+  export const HomeSub = 'Start your mornings with the world\'s best coffees. Try our expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule.'
