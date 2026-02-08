dd569541bd7c4e540317ced7fe1506bd068a5ce1
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-08 15:58:15 -0300

    refactor: add text content to NumData

--- src/components/Data/NumData.ts
+++ src/components/Data/NumData.ts
@@ -1,9 +1,6 @@
-
-
-const t1 = 'Pick your coffee';
+const t1 = 'Pick your coffee'
 const t2 = 'Choose the frequency'
 const t3 = 'Receive and enjoy!'
-
 const d1 = 'Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.';
 const d2 = 'Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.';
 const d3 = 'We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.';
