3ee5ee7be8bd5fa8ad3df6f539a5fc19b7606c3f
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-09 10:59:14 -0300

    fix: fixing testing error on run test

--- src/components/Hero/test/index.test.tsx
+++ src/components/Hero/test/index.test.tsx
@@ -12,8 +12,6 @@ describe('testing if the images files will start or not: ', () => {
    it('should render Hero component with Home images', () => {
     const {mobile, tablet, desk} = HeroBank.Home;
     const {getByRole} = renderWithRouter(<Hero mobile={mobile} tablet={tablet} desk={desk} type='Home' />);
-    
-
     getByRole('img', { name: /mobile-hero/i });
     getByRole('img', { name: /tablet-hero/i });
     getByRole('img', { name: /desktop-hero/i });
@@ -24,10 +22,8 @@ describe('render HeroSection', () => {
    it('should render HeroSection with a type:' , () => {
       renderWithRouter(<HeroSection photoType={HeroBank.Home} type='Home' title='Welcome' 
       subtitle='This is the hero section'  btnActive={true} />);
-
       screen.getByText('Welcome');
       screen.getByText('This is the hero section');
-
-      screen.getByText('Get Started');
+      screen.getByText('create your plan');
    })
 })
