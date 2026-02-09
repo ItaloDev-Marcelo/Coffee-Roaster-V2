dd3eac34c3896c064dd0256b7a90b86fd96e85a7
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-09 14:37:20 -0300

    style: add cursor-pointer to AccordionItem

--- src/components/OrderSummary-accordion/AccordionItem.tsx
+++ src/components/OrderSummary-accordion/AccordionItem.tsx
@@ -7,7 +7,7 @@ const AccordionItem = ({handleAccordion,index,accordionOpen, children, data}:Acc
         <div>
             <div className='header flex flex-row   h-10 space-between items-center my-5' onClick={() => handleAccordion(index)}>
              <div className='flex flex-row justify-between items-center  w-full'>
-                <div className='cursor-pointer'> <p className='mr-2.5 text-1.7xl text-gray-300 font-bold'><span className={accordionOpen == index ? 'text-green-700' : ''}>{data.accordionTitle}</span></p> </div>
+g                <div className='cursor-pointer'> <p className='mr-2.5 text-1.7xl text-gray-500 font-bold'><span className={accordionOpen == index ? 'text-green-700' : ''}>{data.accordionTitle}</span></p> </div>
 
                  <div className={accordionOpen == index ? 
                 'rotate-180 delay-50 duration-300 ease-in-out scale-75 ' :
