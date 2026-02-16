261b138060653fdaee8dcae7561aafc1a21e7ec2
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-16 14:24:36 -0300

    style: add more style to AccordionItem

--- src/components/OrderSummary-accordion/AccordionItem.tsx
+++ src/components/OrderSummary-accordion/AccordionItem.tsx
@@ -5,9 +5,9 @@ const AccordionItem = ({handleAccordion,index,accordionOpen, children, data}:Acc
   return (
     <div>
         <div>
-            <div className='header flex flex-row   h-10 space-between items-center my-5' onClick={() => handleAccordion(index)}>
+            <div className='header flex flex-row   h-10 items-center my-5' onClick={() => handleAccordion(index)}>
              <div className='flex flex-row justify-between items-center  w-full'>
-              <div className='cursor-pointer'> <p className='mr-2.5 text-1.7xl text-gray-500 font-bold'><span className={accordionOpen == index ? 'text-green-700' : ''}>{data.accordionTitle}</span></p> </div>
+              <div className='cursor-pointer'> <p className='mr-2.5 text-1.7xl text-cyan-900 font-bold'><span className={accordionOpen == index ? 'text-green-700' : ''}>{data.accordionTitle}</span></p> </div>
 
                  <div className={accordionOpen == index ? 
                 'rotate-180 delay-50 duration-300 ease-in-out scale-75 ' :
@@ -15,7 +15,7 @@ const AccordionItem = ({handleAccordion,index,accordionOpen, children, data}:Acc
                  <IconArrow /></div>
             </div> 
              </div>
-            <div className={accordionOpen == index ? 'flex  p-5 flex-col lg:flex-row gap-10 h-125 lg:h-60   text-white' : 'hidden'}>
+            <div className={accordionOpen == index ? 'flex  p-5 flex-col lg:flex-row gap-10 space-between h-140  md:h-125 lg:h-70   text-white' : 'hidden'}>
               {children}
             </div>
         </div>
