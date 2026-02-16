e1b154c1464b8ced071700de2afa3704e28a8e5e
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-16 09:24:22 -0300

    style: add more style in CustomIamgeProps

--- src/components/CustomImage/CustomImage.tsx
+++ src/components/CustomImage/CustomImage.tsx
@@ -4,8 +4,8 @@ const CustomImage = ({mobile,tablet,desk}:CustomImageProps) => {
   return (
     <div>
        <img src={mobile} alt='' className='md:hidden w-82.5 rounded-md' />
-        <img src={tablet} alt=''className='hidden md:block lg:hidden rounded-md' />
-         <img src={desk} alt='' className='hidden lg:block rounded-lg' />
+        <img src={tablet} alt=''className='hidden md:block w-100   lg:hidden rounded-md' />
+         <img src={desk} alt='' className='hidden lg:block  rounded-lg' />
     </div>
   )
 }
