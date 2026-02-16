23100d55e3fd4c2d96d9fa70ab2256153b2888a4
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-16 09:25:39 -0300

    style: add more style on style and imgSpace in OurCollectionData

--- src/components/Data/OurCollectionData.ts
+++ src/components/Data/OurCollectionData.ts
@@ -5,33 +5,33 @@ import Danche from '../../assets/images/home/desktop/image-danche.png';
 
 export const OurCollectionData = [
     {
-        style: 'w-[200px] lg:w-[250px] text-center flex flex-col items-center justify-center my-7 lg:my-5 xl:my-0 z-1000' ,
+        style: 'w-[200px] md:w-[300px]  lg:w-[250px] text-center flex flex-col items-center justify-center my-7 xl:my-5  z-1000' ,
         space: false,
-        imgSpace: 'w-[150px] lg:w-[200px] my-2 lg:pb-20 xl:my-5',
+        imgSpace: 'w-[150px] md:w-[170px]  lg:w-[200px] my-2 xl:pb-20 xl:my-5',
         icon:GranEspresso,
         title:'Gran Espress',
         description: 'Light and flavorful blend with cocoa and black pepper for an intense experience'
     },
     {
-         style: 'w-[200px] lg:w-[250px] text-center flex flex-col items-center justify-center my-7 lg:my-5 xl:my-0 z-1000 ',
+         style: 'w-[200px] md:w-[300px]  lg:w-[250px] text-center flex flex-col items-center justify-center my-7 xl:my-5  z-1000 ',
         space: false,
-        imgSpace: 'w-[150px] lg:w-[200px] my-2 lg:pb-20 xl:my-5',
+        imgSpace: 'w-[150px]  md:w-[170px] lg:w-[200px] my-2 xl:pb-20 xl:my-5',
         icon:Planalto,
         title:'Planalto',
         description: 'Brazilian dark roast with rich and velvety body, and hints of fruits and nuts'
     },
     {
-         style: 'w-[200px] lg:w-[250px] text-center flex flex-col items-center justify-center my-7 lg:my-5 xl:my-0 z-1000 ',
+         style: 'w-[200px] md:w-[300px]  lg:w-[250px] text-center flex flex-col items-center justify-center my-7 xl:my-5  z-1000 ',
         space: false,
-        imgSpace: 'w-[150px] lg:w-[200px] my-2 lg:pb-20 xl:my-5',
+        imgSpace: 'w-[150px]  md:w-[170px] lg:w-[200px] my-2 xl:pb-20 xl:my-5',
         icon:Piccollo,
         title:'Piccollo',
         description: 'Mild and smooth blend featuring notes of toasted almond and dried cherry'
     },
     {
-         style: 'w-[200px] lg:w-[250px] text-center flex flex-col items-center justify-center my-7 lg:my-5 xl:my-0 z-1000 ',
+         style: 'w-[200px] md:w-[300px]  lg:w-[250px] text-center flex flex-col items-center justify-center my-7 xl:my-5  z-1000 ',
         space: true,
-        imgSpace: 'w-[150px] lg:w-[200px] my-2 lg:pb-20 xl:my-5',
+        imgSpace: 'w-[150px]  md:w-[170px] lg:w-[200px] my-2 xl:pb-20 xl:my-5',
         icon:Danche,
         title:'Danche',
         description: 'Ethiopian hand-harvested blend densely packed with vibrant fruit notes'
