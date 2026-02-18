62454be35a4888abf9ddb90b16c20a0975ae39af
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-18 14:14:57 -0300

    style: more style

--- src/components/Card/CardType2.tsx
+++ src/components/Card/CardType2.tsx
@@ -4,8 +4,8 @@ import Button from "../Plan-Button/Button"
 const CardType2 = ({num, title, description, hasAButton, style}:CardType2Props) => {
   return (
     <article className={`w-70 relative md:w-68 p-2 lg:p-0 lg:w-75  my-4 lg:my-0 lg:mx-0 ${style}`}>
-         <h3 className='font-bold text-[7rem] lg:text-[6.5rem]'>{num}</h3>
-         <h4 className="text-2xl font-semibold w-37.5 lg:w-75">{title}</h4>
+         <h3 className='font-bold text-[7rem] lg:text-[6.5rem] text-[#ffd4b9]'>{num}</h3>
+         <h4 className="text-2xl font-bold w-37.5 lg:w-75">{title}</h4>
          <p className="my-5">{description}</p>
          {hasAButton && <Button/>}
     </article>
--- src/components/Footer/Container.tsx
+++ src/components/Footer/Container.tsx
@@ -4,7 +4,7 @@ type containerProp = {children: React.ReactNode}
 
 const Container = ({children}:containerProp ) => {
   return (
-    <div className='p-5 md:p-10 md-2:p-10  lg:p-20 mb-90 lg:mb-0'>{children}</div>
+    <div className='py-5 px-2.5 md:p-10 md-2:p-10  lg:p-20 mb-90 lg:mb-0'>{children}</div>
   )
 }
 
