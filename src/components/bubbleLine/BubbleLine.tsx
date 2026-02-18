b460d62bc67c4eadb3977ff7ec5cc4444cdbbdbe
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-18 14:14:11 -0300

    style: change bg-color and width and height

--- src/components/bubbleLine/BubbleLine.tsx
+++ src/components/bubbleLine/BubbleLine.tsx
@@ -2,10 +2,10 @@
 const BubbleLine = () => {
   return (
    <div className='xl:flex-row hidden xl:flex  justify-between items-center w-[50%] relative my-10'>
-     <div className='w-170 h-0.5 bg-pink-400 absolute top-5 left-2 '></div>
-      <div className='w62.5 h-6.5 border relative  bg-white border-cyan-700 rounded-[50%] p-5'></div>
-      <div className='w-6.5 h-6.5 border relative bg-white  left-25 border-cyan-700 rounded-[50%] p-5'></div>
-      <div className='w-6.5 h-6.5 border relative  bg-white left-55 border-cyan-700 rounded-[50%] p-5'></div>
+     <div className='w-170 h-0.5 bg-[#ffd4b9] absolute top-4 left-2 '></div>
+      <div className='w-3.5 h-3.5 border relative  bg-white border-cyan-700 rounded-[50%] p-4'></div>
+      <div className='w-3.5 h-3.5 border relative bg-white  left-25 border-cyan-700 rounded-[50%] p-4'></div>
+      <div className='w-3.5 h-3.5 border relative  bg-white left-55 border-cyan-700 rounded-[50%] p-4'></div>
     </div>
   )
 }
--- src/components/bubbleLine/BubbleLineBlack.tsx
+++ src/components/bubbleLine/BubbleLineBlack.tsx
@@ -3,10 +3,10 @@ const BubbleLineBlack = () => {
   return (
 
      <div className='xl:flex-row hidden xl:flex justify-between items-center w-[40%] relative my-10'>
-     <div className='w-180 h-0.75 bg-white absolute top-5 left-5 '></div>
-      <div className='w-6.5 h-6.5 border relative   border-cyan-700 rounded-[50%] p-5'></div>
-      <div className='w-6.5 h-6.5 border relative   left-34 border-cyan-700 rounded-[50%] p-5'></div>
-      <div className='w-6.5 h-6.5 border relative   left-70 border-cyan-700 rounded-[50%] p-5'></div>
+     <div className='w-180 h-0.75 bg-[#ffd4b9] absolute top-5 left-5 '></div>
+      <div className='w-3.5 h-3.5 border relative   border-cyan-700 rounded-[50%] p-4.5'></div>
+      <div className='w-3.5 h-3.5 border relative   left-34 border-cyan-700 rounded-[50%] p-4.5'></div>
+      <div className='w-3.5 h-3.5 border relative   left-70 border-cyan-700 rounded-[50%] p-4.5'></div>
     </div>
 
     
